e9e8d4aa31ac7a84293619962b0fcc271e87ee85
Abdullahasan515 <166597116+Abdullahasan515@users.noreply.github.com>
2024-08-03 22:43:03 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<head>
+    <meta chrset="utf-8"/>
+    <meta name="keywords" content=""/>
+    <meta name="description" content=""/>
+    <title>project2</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=pt+sans:400,700i&display=swap"/>
+    <link rel="stylesheet" href="css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="./css/style.css"/>
+    <link rel="stylesheet" href="./css/all.css"/>
+</head>
+<body><!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <marquee><img src="coollogo_com-50161028.gif" class="marq"></marquee>
+    <!-- HEADER............................................................................................... -->
+    <header>
+        <div class="container">
+            <div class="header-content col-sm-12">
+                <img src="logo1.png" alt="logo" class="f"/>
+                <img src="coollogo_com-18501831.gif" class="fg" />
+            
+                <nav>
+                    <ul class="list-unstyled ">
+                        <li class="dropdown">
+                            <a class="link-item" href="#">خواتم عقيق</a>
+                            <div class="dropdown-menu" >
+                                <ul class="list-unstyled">
+                                    <li><a href="#">الاحجار المتوفرة</a></li><br>
+                                    <li><a href="#">العقيق اليمني</a></li><br>
+                                    <li><a href="#">الفيروز</a></li><br>
+                                    <li><a href="#">الزمرد</a></li><br>
+                                    <li><a href="#">المرجان</a></li>
+                                </ul>
+                                <ul class="list-unstyled">
+                                    <li><a href="#">الانواع</a></li><br>
+                                    <li><a href="#">سويسري</a></li><br>
+                                    <li><a href="#">تركي</a></li><br>
+                                    <li><a href="#"> ايطالي</a></li><br>
+                                    <li><a href="#">تايلندي</a></li>
+                                </ul>
+                                <ul class="list-unstyled">
+                                    <li><a href="#">العيار</a></li><br>
+                                    <li><a href="#">999</a></li>
+                                    <li><a href="#">925</a></li><br>
+                                    <li><a href="#">925</a></li>
+                                    <li><a href="#">925</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a class="link-item" href="#">أحجار كريمة </a></li>
+                        <li><a class="link-item" href="#">خواتم تايلندي</a></li>
+                        <li><a class="link-item" href="#">خواتم سويسري</a></li>
+                        <li><a class="link-item" href="#">خواتم نسائي</a></li>
+                        <li><a class="link-item" href="#">المزيد</a></li>
+                    </ul>
+                </nav>
+                <div class="login">
+                    <ul class="list-unstyledicon">
+                        <li> <i class="fa fa-search fa-2x"></i></li>
+                        <li> <i class="fa fa-user fa-2x"></i></li>
+                        <li class="e"> <i class="icon fa fa-cart-plus fa-2x"></i>صفحتك</li>
+                    </ul> <!-- /list-unstyled -->
+                </div> <!-- /login -->
+            </div> <!-- header-content -->
+        </div> <!-- /container -->
+    </header> 
---
+++ index.php
@@ -0,0 +1,295 @@
+<?php include "header.php" ;?>
+    <!-- SLIDER............................................................................................... -->
+    <section class="slider1">
+        <video autoplay muted loop id="my-video">
+          <source src="./s.mp4" type="video/mp4" />
+        </video>
+        <div class="video-overlay"></div>
+        <div class="viewtext">
+          <h2 class="textv">يوجد لدينا جميع انواع الخواتم الفضه</h2>
+          <button>
+            <p>عرض</p>
+          </button>
+        </div>
+        <div class="new-element" style="position: relative; z-index: 6;">
+          <!-- محتوى العنصر الجديد -->
+        </div>
+      </section>
+    <!-- CATEGORY............................................................................................. -->
+    <section class="category section-padding">
+        <div class="section-header">
+            <h2 class="section-title">عروض خاصه</h2>
+            <span class="line"></span>
+        </div> <!-- /section-header -->
+        <div class="container">
+            <div class="category-items">
+                <div class="category-item">
+                    <img src="./yemeni1.jpg" alt="this is image"/>
+                    <h4 class="category-item-subtitle">الأكثر شعبية</h4>
+                    <h2 class="category-item-title">العقيق اليماني الداوودي</h2>
+                    <i class="icon fa fa-angle-right fa-2x"></i>
+                </div> <!-- /category-item -->
+                <div class="category-item">
+                    <img src="./yemeni-2.jpg" alt="this is image"/>
+                    <h4 class="category-item-subtitle">الأكثر شعبية</h4>
+                    <h2 class="category-item-title">اليماني الداوودي المصور</h2>
+                    <i class="icon fa fa-angle-right fa-2x"></i>
+                </div> <!-- /category-item -->
+                <div class="category-item">
+                    <img src="./yemeni3.jpg" alt="this is image"/>
+                    <h4 class="category-item-subtitle">الأكثر شعبية</h4>
+                    <h2 class="category-item-title">اليماني الداوودي المصور</h2>
+                    <i class="icon fa fa-angle-right fa-2x"></i>
+                </div> <!-- /category-item -->
+                <div class="category-item">
+                    <img src="./yemeni-4.jpg" alt="this is image" style="height: 39.5vh;"/>
+                    <h4 class="category-item-subtitle">الأكثر شعبيه</h4>
+                    <h2 class="category-item-title">عقيق يماني مصور وجه رجل</h2>
+                    <i class="icon fa fa-angle-right fa-2x"></i>
+                </div> <!-- /category-item -->
+            </div> <!-- /category-items -->
+            <div class="discount">
+                <div class="discount-desc">
+                    <div class="top">
+                        <i class="icon fa fa-quote-left fa-lg"></i>
+                        <h2 class="discount-title">معلومات عن الموقع</h2>
+                        <span class="discount-subtitle">(التسويق)</span>
+                    </div> <!-- ./top -->
+                    <p>هذا في الحقيقه محل يبيع 
+                        خواتم الفضه في منطقه نجران بالمملكه العربيه السعوديه قمت باانشاء موقع لهم  لبيع اجود انواع الاحجار الكريمه والفضه من الدرجه الاولى 
+                        وفرعهم الاساسي في محافظة صنعاء لديهم احجار كريمه بجميع انواعها العقيق اليمني الكبدي و الاحمر والسليماني والداوودي والياقوت و المرجان
+                         والزمرد والكهرمان ولدبهم تفصيل حسب الطلب وحسب النقشه التي تريدها وجيع انواع الفضه السويسري والايطالي  والتايلندي فضه عيار تسعمئة وتسعه وتسعون
+                    </p>
+                    <div class="bullets">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div> <!-- ./bullets -->
+                    </div> <!-- ./discount-desc -->
+                <div class="discount-item">
+                    <div class="discount-item-info">
+                        <h3>30% تخفيض</h3>
+                        <h2>تفصيل على حسب الطلب</h2>
+                    </div> <!-- ./discount-item-info -->
+                </div> <!-- ./discount-item -->
+                <div class="discount-item">
+                    <div class="discount-item-info">
+                        <h3>30% تخفيض</h3>
+                        <h2>تفصيل على حسب الطلب</h2>
+                    </div> <!-- ./discount-item-info -->
+                </div> <!-- ./discount-item -->
+            </div> <!-- ./discount -->
+            <div class="sprites">
+                <div class="sprite-item">
+                    <div class="sprite-icon sprite-icon-1"></div>
+                    <div class="sprite-info">
+                        <h3 style="color:red ;">الياقوت الاحمر</h3>
+                        <span>خاتم ياقوت فضه ايطالي عيار 925</span>
+                    </div> <!-- ./sprite-info -->
+                </div> <!-- ./sprite-icon -->
+                <div class="sprite-item">
+                    <div class="sprite-icon sprite-icon-2"></div>
+                    <div class="sprite-info">
+                        <h3 style="color: green;">زمرد طبيعي</h3>
+                        <span>خاتم ملكي فضه عيار 925</span>
+                    </div> <!-- ./sprite-info -->
+                </div> <!-- ./sprite-icon -->
+                <div class="sprite-item">
+                    <div class="sprite-icon sprite-icon-3"></div>
+                    <div class="sprite-info">
+                        <h3  style="color:orange ;">كهرمان نادر</h3>
+                        <span>خاتم كهرمان فضه سويسري درجه اولى</span>
+                    </div> <!-- ./sprite-info -->
+                </div> <!-- ./sprite-icon -->
+                <div class="sprite-item">
+                    <div class="sprite-icon sprite-icon-4"></div>
+                    <div class="sprite-info">
+                        <h3 style="color: skyblue;">فيروز عروق</h3>
+                        <span>خاتم فيروز سويسري درحه اولى عيار 999</span>
+                    </div> <!-- ./sprite-info -->
+                </div> <!-- ./sprite-icon -->
+                <div class="sprite-item">
+                    <div class="sprite-icon sprite-icon-5">
+                    </div>
+                    <div class="sprite-info">
+                        <h3 style="color:orangered ;">عقيق احمر</h3>
+                        <span>خاتم عقيق درجه اولى والاكثر طلبا</span>
+                    </div> <!-- ./sprite-info -->
+                </div> <!-- ./sprite-icon -->
+            </div> <!-- ./sprites -->
+            <hr>
+        </div> <!-- /container -->
+    </section>
+    <!-- PRODUCTS............................................................................................. -->
+    <section class="products section-padding">
+        <div class="section-header">
+            <h2 class="section-title">الانواع المميزة</h2>
+            <span class="line"></span>
+        </div> <!-- /section-header -->
+        <div class="container">
+            <ul class="list-product list-unstyled ">
+                <li style="color: gainsboro;">خواتم رجاليه</li>
+                <li style="color: gainsboro;">خواتم نسائيه</li>
+                <li style="color: gainsboro;">خواتم بناتي</li>
+                <li style="color: gainsboro;">خواتم ولادي</li>
+            </ul>
+            <div class="products-items">
+                <div class="product-item">
+                    <div class="product-img">
+                        <img src="./end1.jpg" alt="this is image" class="pro"/>
+                        <div class="product-overlay">
+                            <ul class="list-unstyled">
+                                <li><i class="fab fa-facebook-square fa-lg" style="color: blue;"></i></li>
+                                <li><i class="fab fa-twitter-square fa-lg" style="color: dodgerblue;"></i></li>
+                                <li><i class="fab fa-linkedin fa-lg" style="color: aliceblue;"></i></li>
+                                <li><i class="fab fa-instagram-square fa-lg" style="color: deeppink;"></i></li>
+                            </ul>
+                        </div> <!-- ./product-overlay -->
+                        <div class="product-info">
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="fa fa-star-o fa-sm"></i>
+                            <span style="color: aliceblue;">خاتم عقيق يمني كبدي فضه ايطالي عيار 925</span>
+                            <h3 style="font-size: xx-large; color: aquamarine;">
+                            <sub >42$<u style="margin-left: 10px; 
+                            text-decoration-color: red; text-decoration-line: line-through; 
+                            color: white;">55$</u></sub></h3>
+                        </div> <!-- ./product-info -->
+                    </div> <!-- ./product-img -->
+                </div> <!-- ./product-item -->
+                <div class="product-item">
+                    <div class="product-img">
+                        <img src="./end2.webp" alt="this is image" class="pro"/>
+                        <div class="product-overlay">
+                            <ul class="list-unstyled">
+                                <li><i class="fab fa-facebook-square fa-lg" style="color: blue;"></i></li>
+                                <li><i class="fab fa-twitter-square fa-lg" style="color: dodgerblue;"></i></li>
+                                <li><i class="fab fa-linkedin fa-lg" style="color: aliceblue;"></i></li>
+                                <li><i class="fab fa-instagram-square fa-lg" style="color: deeppink;"></i></li>
+                            </ul>
+                        </div> <!-- ./product-overlay -->
+                        <div class="product-info">
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <span style="color: aliceblue;">خاتم فضه ايطالي كريستال عيار 925 </span>
+                            <h3 style="font-size: xx-large; color: aquamarine;">
+                            <sub >19$<u style="margin-left: 10px; 
+                            text-decoration-color: red; text-decoration-line: line-through; 
+                            color: white;">28$</u></sub></h3>
+                        </div> <!-- ./product-info -->
+                    </div> <!-- ./product-img -->
+                </div> <!-- ./product-item -->
+                <div class="product-item">
+                    <div class="product-img">
+                        <img src="./end3.webp" alt="this is image" class="pro"/>
+                        <div class="product-overlay">
+                            <ul class="list-unstyled">
+                                <li><i class="fab fa-facebook-square fa-lg" style="color: blue;"></i></li>
+                                <li><i class="fab fa-twitter-square fa-lg" style="color: dodgerblue;"></i></li>
+                                <li><i class="fab fa-linkedin fa-lg" style="color: aliceblue;"></i></li>
+                                <li><i class="fab fa-instagram-square fa-lg" style="color: deeppink;"></i></li>
+                            </ul>
+                        </div> <!-- ./product-overlay -->
+                        <div class="product-info">
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="fa fa-star-o fa-sm"></i>
+                            <span style="color: aliceblue;">خاتم فضه تايلندي لون اسود عيار 925</span>
+                            <h3 style="font-size: xx-large; color: aquamarine;">
+                            <sub >17$<u style="margin-left: 10px; 
+                            text-decoration-color: red; text-decoration-line: line-through; 
+                            color: white;">25$</u></sub></h3>
+                        </div> <!-- ./product-info -->
+                    </div> <!-- ./product-img -->
+                </div> <!-- ./product-item -->
+            </div> <!-- ./products items -->
+            <div class="products-items">
+                <div class="product-item">
+                    <div class="product-img">
+                        <img src="./design-1.jpg" alt="this is image" class="pro"/>
+                        <div class="product-overlay">
+                            <ul class="list-unstyled">
+                                <li><i class="fab fa-facebook-square fa-lg" style="color: blue;"></i></li>
+                                <li><i class="fab fa-twitter-square fa-lg" style="color: dodgerblue;"></i></li>
+                                <li><i class="fab fa-linkedin fa-lg" style="color: aliceblue;"></i></li>
+                                <li><i class="fab fa-instagram-square fa-lg" style="color: deeppink;"></i></li>
+                            </ul>
+                        </div> <!-- ./product-overlay -->
+                        <div class="product-info">
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="fa fa-star-o fa-sm"></i>
+                            <span style="color: aliceblue;">خاتم عقيق يمني فضه ايطالي عيار925</span>
+                            <h3 style="font-size: xx-large; color: aquamarine;">
+                            <sub >35$<u style="margin-left: 10px; 
+                            text-decoration-color: red; text-decoration-line: line-through; 
+                            color: white;">45$</u></sub></h3>
+                        </div> <!-- ./product-info -->
+                    </div> <!-- ./product-img -->
+                </div> <!-- ./product-item -->
+                <div class="product-item">
+                    <div class="product-img">
+                        <img src="./end5.webp" alt="this is image" class="pro"/>
+                        <div class="product-overlay">
+                            <ul class="list-unstyled">
+                                <li><i class="fab fa-facebook-square fa-lg" style="color: blue;"></i></li>
+                                <li><i class="fab fa-twitter-square fa-lg" style="color: dodgerblue;"></i></li>
+                                <li><i class="fab fa-linkedin fa-lg" style="color: aliceblue;"></i></li>
+                                <li><i class="fab fa-instagram-square fa-lg" style="color: deeppink;"></i></li>
+                            </ul>
+                        </div> <!-- ./product-overlay -->
+                        <div class="product-info">
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="fa fa-star-o fa-sm"></i>
+                            <span style="color: aliceblue;">خاتم فضه ايطالي كريستال عيار925</span>
+                            <h3 style="font-size: xx-large; color: aquamarine;">
+                            <sub >16$<u style="margin-left: 10px; 
+                            text-decoration-color: red; text-decoration-line: line-through; 
+                            color: white;">30$</u></sub></h3>
+                        </div> <!-- ./product-info -->
+                    </div> <!-- ./product-img -->
+                </div> <!-- ./product-item -->
+                <div class="product-item">
+                    <div class="product-img">
+                        <img src="./end6.webp" alt="this is image" class="pro"/>
+                        <div class="product-overlay">
+                            <ul class="list-unstyled">
+                                <li><i class="fab fa-facebook-square fa-lg" style="color: blue;"></i></li>
+                                <li><i class="fab fa-twitter-square fa-lg" style="color: dodgerblue;"></i></li>
+                                <li><i class="fab fa-linkedin fa-lg" style="color: aliceblue;"></i></li>
+                                <li><i class="fab fa-instagram-square fa-lg" style="color: deeppink;"></i></li>
+                            </ul>
+                        </div> <!-- ./product-overlay -->
+                        <div class="product-info">
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="active fa fa-star fa-sm"></i>
+                            <i class="fa fa-star-o fa-sm"></i>
+                            <span style="color: aliceblue;">خاتم تايلندي عيار 925</span>
+                            <h3 style="font-size: xx-large; color: aquamarine;">
+                            <sub >22$<u style="margin-left: 10px; 
+                            text-decoration-color: red; text-decoration-line: line-through; 
+                            color: white;">40$</u></sub></h3>
+                        </div> <!-- ./product-info -->
+                    </div> <!-- ./product-img -->
+                </div> <!-- ./product-item -->
+            </div> <!-- ./products items -->
+        </div> <!-- ./container -->
+    </section>
+ <script>
+document.addEventListener("DOMContentLoaded", function() {
+  var video = document.getElementById("my-video");
+  video.disablePictureInPicture = true;
+});
+ </script>
+</body>
+</html>
